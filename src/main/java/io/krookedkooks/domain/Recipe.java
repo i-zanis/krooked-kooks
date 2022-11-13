6d4fc07c7e735f07730a8d68ef80f6311c09d065
i-zanis <ioanlafazanis@gmail.com>
2022-11-13 23:16:07 +0000

    feat(domain): add Recipe class

---
+++ src/main/java/io/krookedkooks/domain/Recipe.java
@@ -0,0 +1,13 @@
+package io.krookedkooks.domain;
+
+public class Recipe {
+  private String description;
+  public Integer preparationTime;
+  public Integer cookingTime;
+  private Integer portions;
+  public String source;
+  public String url;
+  public  String directions;
+  public Complexity difficulty;
+  public Byte[] image;
+}
