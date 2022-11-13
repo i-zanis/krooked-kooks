b54358eeb6bda0e3941eeb23e93975aa60b6d2af
i-zanis <ioanlafazanis@gmail.com>
2022-11-13 23:08:52 +0000

    initial commit

---
+++ src/main/java/io/krookedkooks/krookedkooks/KrookedKooksApplication.java
@@ -0,0 +1,13 @@
+package io.krookedkooks.krookedkooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrookedKooksApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KrookedKooksApplication.class, args);
+  }
+
+}
---
+++ src/test/java/io/krookedkooks/krookedkooks/KrookedKooksApplicationTests.java
@@ -0,0 +1,13 @@
+package io.krookedkooks.krookedkooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrookedKooksApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
