af348a07ba3b646c4610fe9fec9766143a887517
i-zanis <ioanlafazanis@gmail.com>
2022-11-13 23:58:06 +0000

    feat(domain): add Hibernate annotations Recipe

--- src/main/java/io/krookedkooks/domain/Recipe.java
+++ src/main/java/io/krookedkooks/domain/Recipe.java
@@ -1,13 +1,27 @@
 package io.krookedkooks.domain;
 
+import org.hibernate.annotations.CascadeType;
+
+import javax.persistence.*;
+
+@Entity
 public class Recipe {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
   private String description;
   public Integer preparationTime;
   public Integer cookingTime;
   private Integer portions;
+  // Deleting Recipe, deletes Notes
+  @OneToOne(cascade = CascadeType.ALL)
+  public Notes notes;
   public String source;
   public String url;
-  public  String directions;
+  public String directions;
   public Complexity difficulty;
-  public Byte[] image;
+  @Lob
+  private Byte[] image;
+
 }
