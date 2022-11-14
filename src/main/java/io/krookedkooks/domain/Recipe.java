8b07a6bb369e3a135c68ac97d7efbed59e4a218d
i-zanis <ioanlafazanis@gmail.com>
2022-11-14 21:37:56 +0000

    refactor(domain): add getters/setters in Recipe class

--- src/main/java/io/krookedkooks/domain/Recipe.java
+++ src/main/java/io/krookedkooks/domain/Recipe.java
@@ -1,19 +1,12 @@
 package io.krookedkooks.domain;
 
-import org.hibernate.annotations.CascadeType;
-
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Recipe {
-
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
-  private String description;
   public Integer preparationTime;
   public Integer cookingTime;
-  private Integer portions;
   // Deleting Recipe, deletes Notes
   @OneToOne(cascade = CascadeType.ALL)
   public Notes notes;
@@ -21,7 +14,114 @@ public class Recipe {
   public String url;
   public String directions;
   public Complexity difficulty;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String description;
+  private Integer portions;
+  @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+  private Set<Ingredient> ingredients;
   @Lob
   private Byte[] image;
+
+  public Long getId() {
+    return id;
+  }
+
+  public Recipe setId(Long id) {
+    this.id = id;
+    return this;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public Recipe setDescription(String description) {
+    this.description = description;
+    return this;
+  }
+
+  public Integer getPreparationTime() {
+    return preparationTime;
+  }
+
+  public Recipe setPreparationTime(Integer preparationTime) {
+    this.preparationTime = preparationTime;
+    return this;
+  }
+
+  public Integer getCookingTime() {
+    return cookingTime;
+  }
+
+  public Recipe setCookingTime(Integer cookingTime) {
+    this.cookingTime = cookingTime;
+    return this;
+  }
+
+  public Integer getPortions() {
+    return portions;
+  }
+
+  public Recipe setPortions(Integer portions) {
+    this.portions = portions;
+    return this;
+  }
+
+  public Notes getNotes() {
+    return notes;
+  }
+
+  public Recipe setNotes(Notes notes) {
+    this.notes = notes;
+    return this;
+  }
+
+  public String getSource() {
+    return source;
+  }
+
+  public Recipe setSource(String source) {
+    this.source = source;
+    return this;
+  }
+
+  public String getUrl() {
+    return url;
+  }
+
+  public Recipe setUrl(String url) {
+    this.url = url;
+    return this;
+  }
+
+  public String getDirections() {
+    return directions;
+  }
+
+  public Recipe setDirections(String directions) {
+    this.directions = directions;
+    return this;
+  }
+
+  public Complexity getDifficulty() {
+    return difficulty;
+  }
+
+  public Recipe setDifficulty(Complexity difficulty) {
+    this.difficulty = difficulty;
+    return this;
+  }
+
+  public Byte[] getImage() {
+    return image;
+  }
+
+  public Recipe setImage(Byte[] image) {
+    this.image = image;
+    return this;
+  }
+
 
 }
