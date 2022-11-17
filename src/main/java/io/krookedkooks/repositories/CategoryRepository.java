2b3e403c484df74a5e5effdab0d61d37923078b5
i-zanis <ioanlafazanis@gmail.com>
2022-11-17 22:46:14 +0000

    feat(repository): add CategoryRepository class

--- src/main/java/io/krookedkooks/repositories/CategoryRepository.java
+++ src/main/java/io/krookedkooks/repositories/CategoryRepository.java
@@ -2,6 +2,11 @@ package io.krookedkooks.repositories;
 
 import io.krookedkooks.domain.Category;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+  Optional<Category> findByDescription(String description);
 }
