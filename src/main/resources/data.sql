212dda9dd1a4e6e1a5e5cab35e4382d423d954e6
i-zanis <ioanlafazanis@gmail.com>
2022-11-18 23:28:29 +0000

    chore(): upload dummy data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO category (description) VALUES("Argentinian");
+INSERT INTO category (description) VALUES("American");
+INSERT INTO category (description) VALUES("Spanish");
+INSERT INTO category (description) VALUES("Portuguese");
+INSERT INTO category (description) VALUES("Canadian");
+INSERT INTO measure_unit (description) VALUES('Cup');
+INSERT INTO measure_unit (description) VALUES('ml');
+INSERT INTO recipe (preparationTime, cookingTime) VALUES (5, 10);
